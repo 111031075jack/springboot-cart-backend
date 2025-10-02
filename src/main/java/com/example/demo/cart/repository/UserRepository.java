185bed95a99c9951e5d7f680a9d5558f19a30d4f
111031075jack <111031075@live.asia.edu.tw>
2025-10-02 19:54:57 +0800

    update

--- src/main/java/com/example/demo/cart/repository/UserRepository.java
+++ src/main/java/com/example/demo/cart/repository/UserRepository.java
@@ -4,11 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.cart.model.entity.User;
+
+import jakarta.transaction.Transactional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -20,5 +23,19 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	// 利用原生 sql
 	@Query(value =  "select * from user where username = :name", nativeQuery = true)
 	Optional<User> findByUsernameNative(@Param("name") String name);
+	
+	@Modifying
+	@Transactional
+	@Query(value = "update user set password = :password where id = :id", nativeQuery = true)
+	int updatePasswordByIdNative(@Param("id") Long id, @Param("password") String password);
+	
+	// 透過 PQL (sql 語法 + 物件操作)
+	@Modifying
+	@Transactional
+	@Query(value = "update User u set u.password = :password where u.id = :id")
+	int updatePasswordById(@Param("id") Long id, @Param("password") String password);
+	
+	
+	
 	
 }

4e24824420c6137bdc15a9b5650c3bd21feed964
111031075jack <111031075@live.asia.edu.tw>
2025-10-09 20:52:02 +0800

    update

--- src/main/java/com/example/demo/cart/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/cart/service/impl/UserServiceImpl.java
@@ -1,35 +1,82 @@
 package com.example.demo.cart.service.impl;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.cart.exception.LoginException;
 import com.example.demo.cart.exception.UserNotFoundException;
 import com.example.demo.cart.model.dto.FavoriteProductDTO;
 import com.example.demo.cart.model.dto.FavoriteUserDTO;
 import com.example.demo.cart.model.dto.LoginDTO;
 import com.example.demo.cart.model.dto.UserDTO;
+import com.example.demo.cart.model.entity.User;
+import com.example.demo.cart.repository.ProductRepository;
+import com.example.demo.cart.repository.UserRepository;
 import com.example.demo.cart.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    @Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	@Autowired
+	private ModelMapper modelMapper;
+
+	
 	@Override
 	public UserDTO findByUsername(String username) throws UserNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<User> optUser = userRepository.findFirstByUsername(username);
+		if(optUser.isEmpty()) {
+			throw new UserNotFoundException("查無使用者: " + username);
+		}
+		// 得到 User 物件
+		User user = optUser.get();
+		// 將 User 轉 UserDTO
+		UserDTO userDTO = modelMapper.map(user, UserDTO.class);
+		return userDTO;
 	}
 
 	@Override
-	public UserDTO login(LoginDTO loginDTO) {
-		// TODO Auto-generated method stub
-		return null;
+	public UserDTO login(LoginDTO loginDTO)throws LoginException {
+		// 判斷 username
+		Optional<User> optUser = userRepository.findFirstByUsername(loginDTO.getUsername());
+		if(optUser.isEmpty()) {
+			throw new LoginException("帳號錯誤");
+		}
+		
+		// 得到 User 物件
+		User user = optUser.get();
+		
+		// 判斷 password
+		if(!user.getPassword().equals(loginDTO.getPassword())) {
+			throw new LoginException("密碼錯誤");
+		}
+		
+		// 將 User 轉 UserDTO
+		UserDTO userDTO = modelMapper.map(user, UserDTO.class);
+		return userDTO;
 	}
 
 	@Override
 	public UserDTO saveUser(UserDTO userDTO) {
-		// TODO Auto-generated method stub
-		return null;
+		// 將UserDTO 轉 User
+		User user = modelMapper.map(userDTO, User.class);
+		try {
+			user = userRepository.save(user);
+		}catch (Exception e) {
+			// TODO: handle exception
+		}
+		// 將User 轉 UserDTO
+		userDTO = modelMapper.map(user, UserDTO.class);
+		return userDTO;
 	}
 
 	@Override
